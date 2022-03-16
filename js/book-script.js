0b6ce2982c404e59a292083aefb7c6ebb0bfb725
amine-a11 <mohamedaminehajji11@yahoo.com>
2022-03-16 19:43:08 +0100

    adding number of days between check-in and check-out

--- js/book-script.js
+++ js/book-script.js
@@ -1,10 +1,26 @@
 function handlecid() {
     const cod = document.getElementById("check-out-calender").value;
-    console.log(cod);
     document.getElementById("check-in-calender").setAttribute("max", cod);
+    calc_number_of_nights();
 }
 function handlecod() {
-    const cod = document.getElementById("check-in-calender").value;
-    console.log(cod);
-    document.getElementById("check-out-calender").setAttribute("min", cod);
+    const cid = document.getElementById("check-in-calender").value;
+    document.getElementById("check-out-calender").setAttribute("min", cid);
+    calc_number_of_nights();
+}
+function calc_number_of_nights() {
+    const cod = document.getElementById("check-out-calender");
+    const cid = document.getElementById("check-in-calender");
+    let cod1;
+    let cid1;
+    if (cod !== null) {
+        cod1 = cod.value;
+    }
+    if (cid !== null) {
+        cid1 = cid.value;
+    }
+    if (cod1 !== null && cid1 !== null) {
+        const diffdays = Math.ceil(((new Date(cod1)).getTime() - (new Date(cid1)).getTime()) / (1000 * 60 * 60 * 24));
+        document.getElementById("nb-of-nights").innerHTML = (diffdays ? diffdays : "");
+    }
 }
