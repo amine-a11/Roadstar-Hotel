07a5aee649a24705817ad4e63995c80793158563
amine-a11 <mohamedaminehajji11@yahoo.com>
2022-03-25 14:55:40 +0100

    Reveal Elements On Scroll

--- js/services-script.js
+++ js/services-script.js
@@ -36,3 +36,14 @@ window.onscroll = () => {
 backToTop.onclick = () => {
     window.scrollTo(0, 0);
 }
+
+// Reveal Elements On Scroll
+
+window.addEventListener("scroll", () => {
+    document.querySelectorAll(".reveal").forEach(ele => {
+        const windowheight = window.innerHeight;
+        const revealtop = ele.getBoundingClientRect().top;
+        const revealpoint = 70;
+        (revealtop < windowheight - revealpoint) ? ele.classList.add("active") : ele.classList.remove("active");
+    });
+})
