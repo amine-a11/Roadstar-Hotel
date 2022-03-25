256ba734060efeaca59da084ab853fb24c5f431b
mehrez bey <mahrezbey@gmail.com>
2022-03-25 13:56:26 +0100

    fixing the header

--- js/main-script.js
+++ js/main-script.js
@@ -1,3 +1,23 @@
+//the header
+const nav = document.querySelector('.fixed-nav');
+const header = document.querySelector('header');
+const options ={
+    rootMargin:'-150px'
+}
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            nav.classList.remove('change-status')
+        }
+        else{
+            nav.classList.add('change-status')
+        }
+    })
+} , options)
+
+observer.observe(header)
+
+//-------------
 const productContainers = [...document.querySelectorAll('.product-container')];
 const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
 const preBtn = [...document.querySelectorAll('.pre-btn')];
